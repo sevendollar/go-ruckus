90b6e0f386ceb2c4b71fd6dc8837ca8af992667a
sevendollar <sevendollar@gmail.com>
2021-05-19 15:56:08 +0800

    remove the parentheses

--- const.go
+++ const.go
@@ -4,6 +4,6 @@ const (
 	L2ACL_Restriction_ALLOW = "ALLOW"
 	L2ACL_Restriction_BLOCK = "BLOCK"
 
-	setZoneByNameMethod  = "(*Ruckus).SetZoneByName()"
-	setL2aclByNameMethod = "(*Ruckus).SetL2aclByName()"
+	setZoneByNameMethod  = "(*Ruckus).SetZoneByName"
+	setL2aclByNameMethod = "(*Ruckus).SetL2aclByName"
 )
